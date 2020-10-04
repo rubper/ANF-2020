7dd37e6ff6443551212b9317a3f9b21569653c4b
rubper <rub.per96@gmail.com>
2020-10-04 01:48:06 -0600

    Configuración boostrap4

--- sisanf2020/sisanf2020/settings.py
+++ sisanf2020/sisanf2020/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # aplicaciones personalizadas
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
