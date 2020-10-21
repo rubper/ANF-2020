cac54af690c620c22c9e8d94b4eee5844bc85033
abcrz <vabcgv@outlook.com>
2020-10-20 19:25:50 -0600

    arreglo en el pattern de idGiro en forms.py de giros

--- sisanf2020/giros/forms.py
+++ sisanf2020/giros/forms.py
@@ -3,7 +3,7 @@ from .models import *
 
 class GiroForm(forms.ModelForm):
     idGiro = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control',
-    'placeholder': 'Rango:[1-99]','pattern': '[1-99]+','title': 'Números únicamente','maxlength' : '2'}))
+    'placeholder': 'Rango:[1-99]','pattern': '[0-9]+','title': 'Números únicamente','maxlength' : '2'}))
     class Meta:
         model = Giro
         fields = [
