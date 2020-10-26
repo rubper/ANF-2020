d021d9057f2fba18c37c0e9cb748edb70ec2ec64
gonzalo <gondopalacios@gmail.com>
2020-10-26 02:59:50 -0600

    crud_Empresa

--- sisanf2020/Empresa/urls.py
+++ sisanf2020/Empresa/urls.py
@@ -7,6 +7,7 @@ app_name = 'Empresa'
 urlpatterns = [
 
     path('crear',crear_Empresa.as_view(), name='crear'),
-  
+    path('mostrar/<int:pk>/borrar',eliminar_Empresa.as_view(),name='eliminar'),
     path('mostrar',mostrar_Empresa.as_view(), name='mostrar'),
+    path('editar/<int:pk>',editar_Empreda.as_view(),name='editar'),
 ]
--- sisanf2020/Empresa/views.py
+++ sisanf2020/Empresa/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from .forms import *
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.views.generic import ListView
 from django import forms
 from django.urls import reverse_lazy, reverse
@@ -17,9 +17,23 @@ class crear_Empresa(SuccessMessageMixin,CreateView):
     model= Empresa
     form_class= Empresa_Forms
     template_name= 'Empresa/crear_Empresa.html'
-    success_url = reverse_lazy('Empresa:administrador_Empresas')
+    success_url = reverse_lazy('Empresa:mostrar')
     Success_message = 'Empresa creada con exito'
 
 class mostrar_Empresa(ListView):
     model=Empresa
     template_name = 'Empresa/administrador_Empresas.html'
+
+class editar_Empreda(UpdateView):
+    model = Empresa
+    form_class = Empresa_Forms
+    template_name = 'Empresa/crear_Empresa.html'
+    success_url = reverse_lazy('Empresa:mostrar')
+    success_message = 'Los datos han sido modifcado'
+
+class eliminar_Empresa(DeleteView):
+    model= Empresa
+    form_class = Empresa_Forms
+    success_url = reverse_lazy('Empresa:mostrar')
+    def get_success_url(self):
+        return reverse_lazy('Empresa:mostrar')
