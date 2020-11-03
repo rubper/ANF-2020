6d35bec92ef5792f159c45f2c7591e091520f189
rubper <rub.per96@gmail.com>
2020-11-03 01:13:28 -0600

    Models Paquetes Analisis-Empresa

--- sisanf2020/Analisis/models.py
+++ sisanf2020/Analisis/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from Empresa.models import Empresa, Cuenta
+from datetime import datetime
 
 # Create your models here.
 
@@ -8,3 +10,20 @@ class Ratios(models.Model):
     nomRatio = models.CharField(max_length=50)
     def __str__(self):
         return self.nomRatio
+
+class Analisis(models.Model):
+    idAnalisis = models.AutoField(primary_key=True)
+    idEmpresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    year_analisis = models.PositiveSmallIntegerField(default=yearActual)
+    year_previos = models.PositiveSmallIntegerField()
+
+class LineaDeInforme(models.Model):
+    idLineaInfo = models.AutoField(primary_key=True)
+    idCuenta = models.ForeignKey(Cuenta,on_delete=models.CASCADE)
+    idAnalisis = models.ForeignKey(Analisis,on_delete=models.CASCADE)
+    variacion_horizontal = models.DecimalField(max_digits=11,decimal_places=2)
+    porcentaje_horizontal = models.DecimalField(max_digits=5,decimal_places=4)
+    porcentaje_vertical = models.DecimalField(max_digits=5,decimal_places=4)
+
+def yearActual():
+    return datetime.today().year
--- sisanf2020/Empresa/models.py
+++ sisanf2020/Empresa/models.py
@@ -13,3 +13,11 @@ class Empresa(models.Model):
 
     def __str__(self):
      return  self.rasonsocial
+
+class Cuenta(models.Model):
+    idCuenta = models.AutoField(primary_key=True)
+    idempresa = models.ForeignKey(Empresa,on_delete=models.CASCADE)
+    codigo_cuenta = models.CharField(max_length=13)
+    nombre_cuenta = models.CharField(max_length=100)
+    tipo_cuenta = models.CharField(max_length=25)
+    naturaleza_cuenta = models.CharField(max_length=12)
