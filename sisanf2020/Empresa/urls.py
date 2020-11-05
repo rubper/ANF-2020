0ee628fcc3ca2abeb125841b9fcda76627ae0a2a
rubper <rub.per96@gmail.com>
2020-11-05 17:49:40 -0600

    detalle_de_empresa restore

--- sisanf2020/Empresa/urls.py
+++ sisanf2020/Empresa/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('crear',crear_Empresa.as_view(), name='crear'),
     path('mostrar/<int:pk>/borrar',eliminar_Empresa.as_view(),name='eliminar'),
     path('mostrar',mostrar_Empresa.as_view(), name='mostrar'),
-    path('editar/<int:pk>',editar_Empreda.as_view(),name='editar'),
+    path('editar/<int:pk>',editar_Empreda.as_view(),name='editar'),    
+    path('detalle/<int:pk>/',detalle_Empresa.as_view(),name='detalle'),
 ]
