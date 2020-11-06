b29d0fd6c4c6caa4edba53db52a0c33e3e59ce1e
rubper <rub.per96@gmail.com>
2020-11-06 00:17:20 -0600

    Campos faltantes de modelos + Integra analisis horizontal + migraciones

--- sisanf2020/Analisis/views.py
+++ sisanf2020/Analisis/views.py
@@ -5,6 +5,46 @@ from .resources import RatiosResource
 from django.contrib import messages
 from django.views.generic import ListView
 from django.urls import reverse
+from datetime import datetime
+from Analisis.models import Analisis, LineaDeInforme
+
+#Analisis
+#Horizontal
+def indexAnalisisHorizontal(request, empresa):
+    #Obtiene listado de análisis
+    AnalisisDeEmpresa = Analisis.objects.filter(idEmpresa=empresa)
+    #Obtiene detalle de elementos del listado
+    LineasDeAnalisis = []
+    for analisis in AnalisisDeEmpresa:
+        LineasDeAnalisis += LineaDeInforme.objects.filter(idAnalisis=analisis.idAnalisis)
+    #Obtiene la lista de los ultimos 30 años a partir de hoy (orden descendente)
+    ListaAniosCompleta = []
+    AnioHoy = datetime.today().year
+    for i in range(30):
+        ListaAniosCompleta.append(AnioHoy-i)
+    #Obtener los anios que ya estan usados
+    ListaAniosUsados = []
+    for analisises in AnalisisDeEmpresa.order_by('year_analisis'):
+        if(ListaAniosUsados.count(analisises.year_analisis)<=0):
+            ListaAniosUsados.append(analisises.year_analisis) 
+    #Obtener años que aún no tienen análisis
+    ##Con cada uno de los anios usados
+    for anioUsado in ListaAniosUsados:
+        #Obtener la cantidad de veces que un anio usado se encuentra en la lista completa de años (30 años)
+        cantidadVecesAnioUsado = ListaAniosCompleta.count(anioUsado)
+        #Si esa cantidad es mayor que 0 (es decir, se encuentra más de una vez en la lista)
+        if(cantidadVecesAnioUsado>0):
+            #Repetir en ciclo, la cantidad de veces que se repite el dato en la lista
+            for j in range(cantidadVecesAnioUsado):
+                #Eliminar los datos encontrados
+                ListaAniosCompleta.remove(anioUsado)  
+    #argumentos que se enviarán a la template
+    argumentos = {
+        'listaAnalisis':AnalisisDeEmpresa.order_by('idAnalisis'),
+        'detallesAnalisis':LineasDeAnalisis,
+        'listaAnios':ListaAniosCompleta
+    }
+    return render(request, 'Analisis/AnalisisHorizontal.html', argumentos)
 
 def uploadRatios(request):
     if request.method == 'POST':
