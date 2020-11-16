4ffc7de9d341ba87a9a2ff9c46c01a7d2113d914
rubper <rub.per96@gmail.com>
2020-11-16 03:02:37 -0600

    preparacion para deploy
    se requiere la instalacion de whitenoise por medio de pip
    instalen los requerimientos a partir del requirements.txt del repo

--- sisanf2020/sisanf2020/settings.py
+++ sisanf2020/sisanf2020/settings.py
@@ -52,6 +52,7 @@ AUTH_USER_MODEL = "Usuarios.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 # Activo 'CookieStorage' que nos permite enviar los mensajes de respuesta al Crear, Eliminar y Actualizar un registro
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
